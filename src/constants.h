eb52180c084c575f688426b9874b5ca9279134f3
Draco <thatdracoyt@gmail.com>
2025-03-06 00:46:44 +0500

    Init

---
+++ src/constants.h
@@ -0,0 +1,5 @@
+#pragma once
+
+static const int GAME_ROWS = 20;
+static const int GAME_COLS = 10;
+static const int GAME_CELL_SIZE = 30;
--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,55 @@
 #include <raylib.h>
-#include "ball.h"
+#include "game.h"
+#include "colors.h"
+#include <iostream>
 
-int main() 
+double lastUpdateTime = 0;
+
+bool EventTriggered(double interval)
 {
-    const Color darkGreen = {20, 160, 133, 255};
-    
-    constexpr int screenWidth = 800;
-    constexpr int screenHeight = 600;
-    
-    Ball ball;
-    
-    InitWindow(screenWidth, screenHeight, "My first RAYLIB program!");
-    SetTargetFPS(60);
-    
-    while (!WindowShouldClose())
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval)
     {
-        ball.Update();
-        
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
+int main()
+{
+    InitWindow(500, 620, "Tetris Test");
+    SetTargetFPS(60);
+
+    Font font = LoadFontEx("font/monogram.ttf", 64, 0, 0);
+
+    Game game = Game();
+
+    while (WindowShouldClose() == false)
+    {
+        UpdateMusicStream(game.music);
+        game.HandleInput();
+        if (EventTriggered(0.2))
+        {
+            game.MoveBlockDown();
+        }
         BeginDrawing();
-            ClearBackground(darkGreen);
-            ball.Draw();
+        ClearBackground(darkBlue);
+        DrawTextEx(font, "Score", {357, 15}, 38, 2, WHITE);
+        DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE);
+        if (game.gameOver)
+        {
+            DrawTextEx(font, "GAME OVER", {320, 450}, 38, 2, WHITE);
+        }
+        DrawRectangleRounded({320, 60, 170, 60}, 0.3, 6, lightBlue);
+        DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, lightBlue);
+        char scoreText[10];
+        sprintf(scoreText, "%d", game.score);
+        Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
+        DrawTextEx(font, scoreText, {320 + (170 - textSize.x) / 2, 65}, 38, 2, WHITE);
+        game.Draw();
         EndDrawing();
     }
-    
+
     CloseWindow();
 }
