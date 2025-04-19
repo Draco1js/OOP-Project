ab682a21a0a52e5e50f9349c8aa0a796902c1e62
Draco <thatdracoyt@gmail.com>
2025-04-19 22:41:41 +0500

    SFML SHIFT

--- src/constants.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-static const int GAME_ROWS = 20;
-static const int GAME_COLS = 10;
-static const int GAME_CELL_SIZE = 30;
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,21 @@
-#include <raylib.h>
-#include "game.h"
-#include "colors.h"
-#include <iostream>
-
-double lastUpdateTime = 0;
-
-bool EventTriggered(double interval)
-{
-    double currentTime = GetTime();
-    if (currentTime - lastUpdateTime >= interval)
-    {
-        lastUpdateTime = currentTime;
-        return true;
-    }
-    return false;
-}
+#include <SFML/Graphics.hpp>
 
 int main()
 {
-    InitWindow(500, 620, "Tetris Test");
-    SetTargetFPS(60);
+    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "CMake SFML Project");
+    window.setFramerateLimit(144);
 
-    Font font = LoadFontEx("font/monogram.ttf", 64, 0, 0);
-
-    Game game = Game();
-
-    while (WindowShouldClose() == false)
+    while (window.isOpen())
     {
-        UpdateMusicStream(game.music);
-        game.HandleInput();
-        if (EventTriggered(0.2))
+        while (const std::optional event = window.pollEvent())
         {
-            game.MoveBlockDown();
+            if (event->is<sf::Event::Closed>())
+            {
+                window.close();
+            }
         }
-        BeginDrawing();
-        ClearBackground(darkBlue);
-        DrawTextEx(font, "Score", {357, 15}, 38, 2, WHITE);
-        DrawTextEx(font, "Next", {370, 175}, 38, 2, WHITE);
-        if (game.gameOver)
-        {
-            DrawTextEx(font, "GAME OVER", {320, 450}, 38, 2, WHITE);
-        }
-        DrawRectangleRounded({320, 60, 170, 60}, 0.3, 6, lightBlue);
-        DrawRectangleRounded({320, 215, 170, 180}, 0.3, 6, lightBlue);
-        char scoreText[10];
-        sprintf(scoreText, "%d", game.score);
-        Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
-        DrawTextEx(font, scoreText, {320 + (170 - textSize.x) / 2, 65}, 38, 2, WHITE);
-        game.Draw();
-        EndDrawing();
-    }
 
-    CloseWindow();
+        window.clear();
+        window.display();
+    }
 }
