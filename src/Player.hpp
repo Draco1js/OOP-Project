4b4ff95954e413c2f9e1d5bbe7a164192f526cf1
Draco <thatdracoyt@gmail.com>
2025-04-20 05:33:11 +0500

    CI v0.0.1
    Co-authored-by: Zumair Shamsi <zumairshamsi@gmail.com>
    Co-authored-by: Devish Kumar <devishkumar1234@gmail.com>

--- src/Player.hpp
+++ src/Player.hpp
@@ -28,7 +28,7 @@ private:
     sf::Vector2f velocity{0.0f, 0.0f};
     float gravity = 0.5f;
     float jumpForce = -12.0f;
-    float moveSpeed = 15.0f;
+    float moveSpeed = 100.0f;
     float groundLevel = 400.0f; // Default ground level
     bool isOnGround = true;
 
